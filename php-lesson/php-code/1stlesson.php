c5f4e3fb20b860a467d3617c41e27e06aa9adc6f
k2mi yagi <connvoykagi@gmail.com>
2012-04-09 11:47:51 +0900

    Add php-lesson

---
+++ php-lesson/php-code/1stlesson.php
@@ -0,0 +1,60 @@
+<?php
+  /**
+  * print
+  */
+  print "Hello Messy!";
+  echo "Hello Messy!";
+
+  /**
+   * var_dump
+   * print_r
+   */
+  $array = array(1,2,3);
+  var_dump($array);
+
+  #this is comment
+  /**
+    this is also comment
+   */
+
+
+
+  /**
+   * declare variable
+   * number, string
+   */
+  $i=1;
+  $a='string';
+ 
+  /**
+   * number,
+   * four arithmetic operations
+   */
+  $i = 1 + 1;
+  $i = 1 - 1;
+  $i = 1 * 1;
+  $i = 1 / 2;
+  $i = 3 % 2;
+
+
+  /**
+   * increment, decrement
+   */
+  $i++;
+  $i--;
+
+
+  /**
+   * control statement
+   * if,else
+   */
+  if($i==1){
+      echo 'i is 1'; 
+  }
+
+  if($i==1){
+      echo 'i is 1'; 
+  }else{
+      echo 'i is not 1'; 
+  }
+?>
---
+++ php-lesson/php-hw/1sthomework.php
@@ -0,0 +1,18 @@
+<?php
+  /**
+   * There are 4 values, $m, $e, $s, $y.
+   * When they are 3multiple number, you print 'Fizz';  
+   * hint:
+   *    What is 9 over 3 ? 
+   *    So When 3multiple number devided 3, remainder is ??? 
+   */
+
+
+  $m=6;
+  $e=7;
+  $s=8;
+  $y=9;
+  
+
+
+?>
