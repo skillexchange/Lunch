a01c2fc153bc34f750265d77d0410cdb094fceee
Hide <takewick@gmail.com>
2012-04-13 02:14:06 +0900

    added for but doesn't work

--- php-lesson/php-hw/1sthomework.php
+++ php-lesson/php-hw/1sthomework.php
@@ -1,10 +1,10 @@
 <?php
   /**
    * There are 4 values, $m, $e, $s, $y.
-   * When they are 3multiple number, you print 'Fizz';  
+   * When they are multiples of 3, print 'Fizz';  
    * hint:
-   *    What is 9 over 3 ? 
-   *    So When 3multiple number devided 3, remainder is ??? 
+   *    What is 9 divided by 3 ? 
+   *    So when a number is not multiples of 3, remainder is ??? 
    */
 
 
@@ -13,6 +13,19 @@
   $s=8;
   $y=9;
   
+//  $i=5%2;
+//  print $i."\n";
 
+  $messy = array($m,$e,$s,$y);
+  var_dump($messy); 
 
+  echo $messy[1]."\n";
+
+  for ($i=0; $i<$messy.length; $i++){
+    if($messy[i] % 3 == 0){
+      echo 'Fizz';
+    }else{
+      echo 'non-Fizz';
+    }
+  }  
 ?>
