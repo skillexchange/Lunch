939778ff4bc93078fdfa4efea610e9b933c4e0cb
Hide <takewick@gmail.com>
2012-05-14 13:49:36 +0900

    3rd hw done

--- php-lesson/php-hw/3rdhomework.php
+++ php-lesson/php-hw/3rdhomework.php
@@ -5,15 +5,26 @@
     と出力してくだい。
   */
 
-  $array1 = array(1,2,3,4,5);
-  foreach ($array1 as $value1){
-  }
-
-  $i=0;
+  $s=0;
   $array2 = array(6,7,8,9,10);
   foreach ($array2 as $value2){
-    echo 4*$array2[4]+$array1[$i]."\n";
-    $i++;
-  }
+    $s=$s+$value2;
+  }  
+ 
+  $array1 = array(1,2,3,4,5);
+  foreach ($array1 as $value1){
+    $h=$s+$value1;
+    echo $h."\n";
+  } 
 
+  $array1 = array(1,2,3,4,5);
+  foreach ($array1 as $value1){
+    $s=0;
+    $array2 = array(6,7,8,9,10);
+      foreach ($array2 as $value2){
+      $s=$s+$value2;
+      }  
+    $h=$s+$value1;
+    echo $h."\n";
+  } 
 ?>
