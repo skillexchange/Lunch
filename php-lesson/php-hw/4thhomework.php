8eef341bb52eb2b71ee5bbccfadba271a8c452ef
Hide <takewick@gmail.com>
2012-05-23 00:53:40 +0900

    can not fix the error on line 14

---
+++ php-lesson/php-hw/4thhomework.php
@@ -0,0 +1,27 @@
+<?php
+  /**
+  * array(3,2,1);
+  * この配列を昇順に並び替えてください
+  *
+  * array(3,2) -> array(2,3)
+  * */
+
+  $array = array(3,2,1);
+  for ($i=0; $i<count($array); $i++){
+    if ($array[$i] > $array[$i+1]){
+      $a = $array[$i];
+      $array[$i] = $array[$i+1];
+      $array[$i+1] = $a;
+    }
+  }
+  
+  var_dump ($array);
+
+
+
+  /**
+  * array(10,3,2,4,1,14,9,7,8)
+  * この配列を昇順に並び替えてください
+  * */
+
+?>
