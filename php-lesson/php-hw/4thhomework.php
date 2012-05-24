d156cfa0d7ccd0414b9e14dab9a4fa6066e9de73
Hide <takewick@gmail.com>
2012-05-25 00:47:39 +0900

    1st one done

--- php-lesson/php-hw/4thhomework.php
+++ php-lesson/php-hw/4thhomework.php
@@ -1,6 +1,5 @@
 <?php
   /**
-<<<<<<< HEAD
   * array(3,2,1);
   * この配列を昇順に並び替えてください
   *
@@ -8,12 +7,19 @@
   * */
 
   $array = array(3,2,1);
-  for ($i=0; $i<count($array); $i++){
+  for ($i=0; $i<count($array)-1; $i++){
     if ($array[$i] > $array[$i+1]){
       $a = $array[$i];
       $array[$i] = $array[$i+1];
       $array[$i+1] = $a;
     }
+  }
+  for ($i=0; $i<count($array)-1; $i++){
+    if ($array[$i] > $array[$i+1]){
+      $a = $array[$i];
+      $array[$i] = $array[$i+1];
+      $array[$i+1] = $a;
+    }   
   }
   
   var_dump ($array);
@@ -24,17 +30,5 @@
   * array(10,3,2,4,1,14,9,7,8)
   * この配列を昇順に並び替えてください
   * */
-=======
-    array(3,2,1);
-    この配列を昇順に並び替えてください
-
-    array(3,2) -> array(2,3)
-  */
-
-  /**
-    array(10,3,2,4,1,14,9,7,8)
-    この配列を昇順に並び替えてください
-  */
->>>>>>> cddedf1939eb3badd0cdcc0cc8b23eaade0fd18c
 
 ?>
