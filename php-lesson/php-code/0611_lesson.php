d9cab4a9ff29a321c849c244081f2884d93ccf0f
Hide <takewick@gmail.com>
2012-06-25 00:50:53 +0900

    done\!

--- php-lesson/php-code/0611_lesson.php
+++ php-lesson/php-code/0611_lesson.php
@@ -3,12 +3,30 @@
   $subject = "abcdef";
   $pattern = '/^a/';
   preg_match($pattern, $subject, $matches);
-  print_r($matches);
+//  print_r($matches);
 //  print($matches);
 
+
   $subject2 = "http://www.yahoo.co.jp/top/kyagi.jpg";
-  $pattern2 = "/www.yahoo.co.jp/";
-  preg_match($pattern2, $subject2, $matches2);
-  print_r($matches2); //=> "www.yahoo.co.jp"
+  $subject3 = "http://www.yahoo.com/top/kyagi.jpg";
+
+//  $pattern_all = '/www/';  // => "www"
+//  $pattern_all = '/[^\/]*/';  // => "http:"
+//  $pattern_all = '/[^w]*/';  // => "http://"
+
+//=> "www.yahoo.co.jp"
+//=> "www.yahoo.com"
+//  $pattern_all = '/w[^\/]*/';  
+
+// without using "w"
+// match "kyagi.jpg"
+  $pattern_all = '/k[^\/]*/';
+//  $pattern_all = '/k[^\.]*/';
+  
+  preg_match($pattern_all, $subject2, $matches2);
+  preg_match($pattern_all, $subject3, $matches3);
+  print_r($matches2); 
+  print_r($matches3); 
+
 
 ?>
