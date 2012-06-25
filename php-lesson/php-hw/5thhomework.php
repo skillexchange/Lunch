2d767b50d07ec8d39ca64fbe03f27c8cde73c08f
Hide <takewick@gmail.com>
2012-06-26 00:42:57 +0900

    finised homework yo

--- php-lesson/php-hw/5thhomework.php
+++ php-lesson/php-hw/5thhomework.php
@@ -18,8 +18,39 @@
  *
  */
 
-echo '<form action="" method="GET">'.
-     '<input type="submit" name="get" value="get request">'.
-     '</form>';
+  $niku='<a href="http://www.flickr.com/photos/kyagyao/2727717819/" title="NEC_0990.jpg by kyagyao, on Flickr"><img src="http://farm4.staticflickr.com/3132/2727717819_96645dc166.jpg" width="500" height="375" alt="NEC_0990.jpg"></a>';
+  $hawaii='<a href="http://www.flickr.com/photos/kyagyao/7168848025/" title="infinity pool by kyagyao, on Flickr"><img src="http://farm9.staticflickr.com/8006/7168848025_4e45474fa1.jpg" width="500" height="333" alt="infinity pool"></a>';
+  $konnkatu='<a href="http://www.flickr.com/photos/take-wick/7424719998/" title="かまーたさん、ノリまくり by takewick, on Flickr"><img src="http://farm8.staticflickr.com/7248/7424719998_79ef626da1.jpg" width="500" height="374" alt="かまーたさん、ノリまくり"></a>';
+ 
+  echo '<form action="" method="GET">'.
+       '<input type="text" name="request">'.
+       '<input type="submit" name="get" value="get request">'.
+       '</form>';
+
+  if ($_GET["request"] == "niku"){ 
+    echo $niku.'<br>';
+  }else if ($_GET["request"] == "hawaii"){
+    echo $hawaii.'<br>';
+  }else if ($_GET["request"] == "konnkatu"){
+    echo $konnkatu.'<br>';
+  }
+ 
+  echo $_GET["request"].'<br><br>';
+
+
+  echo '<form action="" method="POST">'.
+       '<input type="text2" name="request2">'.
+       '<input type="submit" name="post" value="post request">'.
+       '</form>';
+
+  if ($_POST["request2"] == "niku"){
+    echo $niku.'<br>';
+  }else if ($_POST["request2"] == "hawaii"){
+    echo $hawaii.'<br>';
+  }else if ($_POST["request2"] == "konnkatu"){
+    echo $konnkatu.'<br>';
+  }  
+  
+  echo $_POST["request2"];
 
 ?>
