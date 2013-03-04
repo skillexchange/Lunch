2f533769cf269e43749249e0881d93554659b2e2
Hide <takewick@gmail.com>
2013-03-05 01:59:45 +0900

    simplified index.php

--- facebook/login.php
+++ facebook/login.php
@@ -14,4 +14,15 @@
   echo $user;
   echo '<html><body><a href="'.$url.'">URL</a><br></body></html>';
 
+ // Request multi query
+   $fqlResults = $facebook->api(array(
+     'method' => 'fql.multiquery',
+     'queries' =>array(
+     'q1'=> "SELECT user_id, object_id, object_type FROM like WHERE user_id= me()",
+//     'q2'=> "SELECT owner, src_small, src_big, created, object_id FROM photo WHERE object_id IN (SELECT object_id FROM #q1)",
+//     'q3'=> "SELECT username, uid FROM user WHERE uid IN (SELECT owner FROM #q2)"
+   )));
+ 
+  print_r($fqlResults);
+
 ?>
--- php-lesson/php-hw/9thhomework.php
+++ php-lesson/php-hw/9thhomework.php
@@ -1,14 +1,32 @@
 <?php
 
+echo $argv[0];
+echo $argv[1];
+echo $argv[2];
+
+
 try {
   $pdo = new PDO("mysql:host=localhost; dbname=test;unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock","root","root");
 //  $stmt = $pdo->query("SELECT*FROM team_messy");
-//  $stmt1 = $pdo->query("SELECT first_name FROM team_messy WHERE status = pretty good");
-  $stmt1 = $pdo->query("SELECT first_name FROM team_messy");
+  $stmt1 = $pdo->query("SELECT first_name FROM team_messy WHERE status = 'pretty good'");
 
   while($row = $stmt1->fetch(PDO::FETCH_ASSOC)){
     echo implode(",",$row) . PHP_EOL;
   }
+
+  $pdo->query("INSERT INTO team_messy (first_name) VALUES ('yusuke')");
+  $stmt2 = $pdo->query("SELECT first_name FROM team_messy");
+
+  while($row = $stmt2->fetch(PDO::FETCH_ASSOC)){
+    echo implode(",",$row) . PHP_EOL;
+  }
+ 
+  $pdo->query("DELETE FROM team_messy WHERE first_name = 'yusuke'");
+  $stmt3 = $pdo->query("SELECT first_name FROM team_messy");
+ 
+  while($row = $stmt3->fetch(PDO::FETCH_ASSOC)){
+    echo implode(",",$row) . PHP_EOL;
+  } 
 
 } 
 
